4532412fce642a36afd14347163c1bed47733211
Roberto <roberto.cantu1997@gmail.com>
2022-10-25 10:44:25 -0500

    Add amazon bucket

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,40 @@
 import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
+import fs from 'fs';
+
+import AWS from 'aws-sdk';
+import * as dotenv from 'dotenv'
+
+dotenv.config()
 
 const port: number = 3001;
 
+console.log(process.env.AWS_ACCESS_KEY)
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+});
+
+const fileName = 'test.txt'
+
+const uploadFile = () => {
+    fs.readFile(fileName, (err, data) => {
+       if (err) throw err;
+       const params = {
+           Bucket: 'accenturechallengetec', // pass your bucket name
+           Key: 'test.txt', // file will be saved as testBucket/contacts.csv
+           Body: JSON.stringify(data, null, 2)
+       };
+       s3.upload(params, function(s3Err:any, data:any) {
+           if (s3Err) throw s3Err
+           console.log(`File uploaded successfully at ${data.Location}`)
+       });
+    });
+  };
+
+  uploadFile();
 app.get('/index', (req: Request, res: Response) => {
     res.send('Server running...');
 });
