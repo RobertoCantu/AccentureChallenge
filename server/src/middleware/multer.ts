5046936216d6b319a7b45b20606b19003ce45af6
Alam Sanchez <asancheztorres1905@gmail.com>
2022-10-25 16:28:25 -0500

    Add S3 file upload & utils

--- server/src/middleware/multer.ts
+++ server/src/middleware/multer.ts
@@ -1,11 +1,11 @@
 import multer from 'multer';
 import multerS3 from 'multer-s3';
-import { s3 } from './s3-utills';
+import { MAIN_BUCKET, s3 } from '../s3-utills';
 
 export const s3FileUpload = multer({
     storage: multerS3({
         s3,
-        bucket: 'accenturechallengetec',
+        bucket: MAIN_BUCKET,
         key: function (req, file, cb) {
             cb(null, Date.now().toString());
         },
--- server/src/s3-utills.ts
+++ server/src/s3-utills.ts
@@ -1,17 +1,23 @@
-import { S3Client } from '@aws-sdk/client-s3';
+import { S3 } from '@aws-sdk/client-s3';
 
-import assert from 'assert';
+export const MAIN_BUCKET = 'accenturechallengetec';
 
-assert.ok(process.env.AWS_ACCESS_KEY, 'AWS Access Key is not set in env file');
-assert.ok(
-    process.env.AWS_SECRET_ACCESS_KEY,
-    'AWS Access Key is not set in env file'
-);
+if (!process.env.AWS_ACCESS_KEY)
+    throw new Error('AWS Access Key is not set in env file');
+if (!process.env.AWS_SECRET_ACCESS_KEY)
+    throw new Error('AWS Secret access Key is not set in env file');
 
-export const s3 = new S3Client({
+export const s3 = new S3({
     region: process.env.AWS_S3_REGION ?? 'us-east-1',
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY ?? '',
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY ?? '',
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     },
 });
+
+export function deleteObject(key: string) {
+    s3.deleteObject({
+        Bucket: MAIN_BUCKET,
+        Key: key,
+    });
+}
