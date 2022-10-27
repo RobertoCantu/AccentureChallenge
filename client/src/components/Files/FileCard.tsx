37766b54f2dcf65448893d5da18fb5b069bacd0e
Roberto <roberto.cantu1997@gmail.com>
2022-10-27 15:54:13 -0500

    Add styles to file cards

--- client/src/components/Files/FileCard.tsx
+++ client/src/components/Files/FileCard.tsx
@@ -3,8 +3,11 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
-import { Button, CardActionArea, CardActions, Icon } from "@mui/material";
+import { Button, CardActionArea, CardActions, Icon, Grid } from "@mui/material";
 import FolderIcon from "@mui/icons-material/Folder";
+import DeleteIcon from "@mui/icons-material/Delete";
+import IconButton from "@mui/material/IconButton";
+import EditIcon from "@mui/icons-material/Edit";
 
 interface CardType {
 	name: string;
@@ -12,20 +15,23 @@ interface CardType {
 
 function FileCard({ name }: CardType) {
 	return (
-		<Card sx={{ maxWidth: 345 }}>
+		<Card sx={{ maxWidth: 345, height: "100%" }}>
 			<CardActionArea>
 				<CardContent>
 					<FolderIcon fontSize="large" />
 					<Typography gutterBottom variant="h5" component="div">
 						{name}
 					</Typography>
+					<Grid container direction="row" justifyContent="flex-end">
+						<IconButton size="small" color="primary">
+							<EditIcon />
+						</IconButton>
+						<IconButton size="small" color="error">
+							<DeleteIcon />
+						</IconButton>
+					</Grid>
 				</CardContent>
 			</CardActionArea>
-			<CardActions>
-				<Button size="small" color="primary">
-					Share
-				</Button>
-			</CardActions>
 		</Card>
 	);
 }
--- client/src/layouts/index.tsx
+++ client/src/layouts/index.tsx
@@ -1,83 +1,80 @@
 // Routing
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
 
 // Material UI
 import {
-    styled,
-    useTheme,
-    Container,
-    Box,
-    AppBar,
-    Toolbar,
-    Typography,
-    Menu,
-    MenuItem,
-    IconButton,
-} from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+	styled,
+	useTheme,
+	Container,
+	Box,
+	AppBar,
+	Toolbar,
+	Typography,
+	Menu,
+	MenuItem,
+	IconButton,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
-import * as React from 'react';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import List from '@mui/material/List';
-import Divider from '@mui/material/Divider';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/MoveToInbox';
-import MailIcon from '@mui/icons-material/Mail';
+import * as React from "react";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import List from "@mui/material/List";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
 
 // Components
-import DashboardSidebar from './DashboardSidebar';
-import DashboardNavbar from './DashboardNavbar';
-import { margin } from '@mui/system';
-import { ClassNames } from '@emotion/react';
-import TestNavBar from './testNavBar';
-import TestSidebar from './testSidebar';
-import { useFolder } from '../hooks/useFolder';
+import DashboardSidebar from "./DashboardSidebar";
+import DashboardNavbar from "./DashboardNavbar";
+import { margin } from "@mui/system";
+import { ClassNames } from "@emotion/react";
+import TestNavBar from "./testNavBar";
+import TestSidebar from "./testSidebar";
+import { useFolder } from "../hooks/useFolder";
 
-const RootStyle = styled('div')({
-    display: 'flex',
-    minHeight: '100%',
-    flexWrap: 'wrap',
-    //overflow: 'hidden'
+const RootStyle = styled("div")({
+	display: "flex",
+	minHeight: "100%",
+	flexWrap: "wrap",
+	//overflow: 'hidden'
 });
 
 const DRAWER_WIDTH = 280;
 const drawerWidth = 240;
 
-const MainStyle = styled('div')(({ theme }) => ({
-    flexGrow: 1,
-    overflow: 'auto',
-    minHeight: '100%',
-    paddingTop: theme.spacing(12),
-    paddingBottom: theme.spacing(8),
-    paddingLeft: theme.spacing(2),
-    paddingRight: theme.spacing(2),
-    // marginLeft: '180px',
-    //width: `calc(100% - ${DRAWER_WIDTH}px)`
+const MainStyle = styled("div")(({ theme }) => ({
+	flexGrow: 1,
+	overflow: "auto",
+	minHeight: "100%",
+	paddingTop: theme.spacing(12),
+	paddingBottom: theme.spacing(8),
+	paddingLeft: theme.spacing(2),
+	paddingRight: theme.spacing(2),
+	// marginLeft: '180px',
+	//width: `calc(100% - ${DRAWER_WIDTH}px)`
 }));
 
-const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
+const Offset = styled("div")(({ theme }) => theme.mixins.toolbar);
 
 export default function DashboardLayout() {
-    const theme = useTheme();
-    const pages = ['Inicio'];
-    const currentFolder = useFolder();
-    console.log(currentFolder);
-    //const { collapseClick } = useCollapseDrawer();
+	const theme = useTheme();
+	const pages = ["Inicio"];
+	const currentFolder = useFolder();
+	console.log(currentFolder);
+	//const { collapseClick } = useCollapseDrawer();
 
-    return (
-        <Box sx={{ display: 'flex' }}>
-            <DashboardNavbar />
-            <DashboardSidebar />
-            <MainStyle>
-                <Outlet />
-                <div>
-                  hola
-                </div>
-            </MainStyle>
-        </Box>
-    );
+	return (
+		<Box sx={{ display: "flex" }}>
+			<DashboardNavbar />
+			<DashboardSidebar />
+			<MainStyle>
+				<Outlet />
+			</MainStyle>
+		</Box>
+	);
 }
