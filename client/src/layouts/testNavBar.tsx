8c5fa2e23d396e7164bac370061fc82374d4c017
Roberto <roberto.cantu1997@gmail.com>
2022-10-27 00:04:00 -0500

    Add indentation

--- client/src/layouts/testNavBar.tsx
+++ client/src/layouts/testNavBar.tsx
@@ -1,24 +1,23 @@
-import React from 'react'
-import {AppBar, Toolbar, Typography, Box} from '@mui/material';
+import React from "react";
+import { AppBar, Toolbar, Typography, Box } from "@mui/material";
 
 const drawerWidth = 240;
 
 function testNavBar() {
-  return (
-    <Box>
-    <AppBar
-        position="fixed"
-        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-      >
-        <Toolbar>
-          <Typography variant="h6" noWrap component="div">
-            Permanent drawer
-          </Typography>
-        </Toolbar>
-      </AppBar>
-
-      </Box>
-  )
+	return (
+		<Box>
+			<AppBar
+				position="fixed"
+				sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
+			>
+				<Toolbar>
+					<Typography variant="h6" noWrap component="div">
+						Permanent drawer
+					</Typography>
+				</Toolbar>
+			</AppBar>
+		</Box>
+	);
 }
 
-export default testNavBar
+export default testNavBar;
