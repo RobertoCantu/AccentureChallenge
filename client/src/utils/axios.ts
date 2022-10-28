393d61188924c70178b3e41c09e2d7ff58ff4d68
Alam Sanchez <asancheztorres1905@gmail.com>
2022-10-28 11:58:08 -0500

    Add auth token to all requests

--- client/src/utils/axios.ts
+++ client/src/utils/axios.ts
@@ -1,26 +1,41 @@
-import axios from "axios";
+import axios from 'axios';
 
 const axiosInstance = axios.create({
-	// Do something before request is sent
-	baseURL: process.env.REACT_APP_API_URL,
-	headers: {
-		"Content-Type": "application/json",
-	},
-	responseType: "json",
+    // Do something before request is sent
+    baseURL: process.env.REACT_APP_API_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    responseType: 'json',
 });
 
-axiosInstance.interceptors.response.use(
-	(response) => response,
-	(error) => {
-		console.log("aca", JSON.stringify(error));
-		if (error.response && error.response.status == 401) {
-			window.location.assign("/auth/logout");
-		}
+axiosInstance.interceptors.request.use(
+    (config) => {
+        if (config.headers) {
+            config.headers['Authorization'] = `Bearer ${localStorage.getItem(
+                'accessToken'
+            )}`;
+        }
 
-		return Promise.reject(
-			(error.response && error.response.data) || "Something went wrong"
-		);
-	}
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
+
+axiosInstance.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        console.log('aca', JSON.stringify(error));
+        if (error.response && error.response.status == 401) {
+            // window.location.assign("/auth/logout");
+            window.localStorage.removeItem('accessToken');
+            window.localStorage.removeItem('user');
+        }
+
+        return Promise.reject(
+            (error.response && error.response.data) || 'Something went wrong'
+        );
+    }
 );
 
 export default axiosInstance;
--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -1,21 +1,28 @@
-import type { Request, Response, NextFunction } from "express";
-import jwt from "jsonwebtoken"
+import type { Request, Response, NextFunction } from 'express';
+import jwt from 'jsonwebtoken';
 
-export const isAuthenticated = async (request: Request, response: Response, next: NextFunction ) => {
+export const isAuthenticated = async (
+    request: Request,
+    response: Response,
+    next: NextFunction
+) => {
+    console.log('getting req', request.path);
     try {
-        const token = request.get("Authorization")?.split(" ")[1];
+        const token = request.get('Authorization')?.split(' ')[1];
 
-        if(token) {
-            const decodedToken = jwt.verify(token, process.env.TOKEN_SECRET as string);
-            request.user = {id: (<any>decodedToken).userId};
+        if (token) {
+            const decodedToken = jwt.verify(
+                token,
+                process.env.TOKEN_SECRET as string
+            );
+            request.user = { id: (<any>decodedToken).userId };
             next();
         } else {
-            response.status(404).send("Token de acceso inválido");
+            response.status(401).send('Token de acceso inválido');
             return;
         }
-
     } catch (error: any) {
-        return response.status(500).json(error.message)
+        return response.status(401).json(error.message);
     }
 };
 
